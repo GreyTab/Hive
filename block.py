9d13a3afd03b65dd1a3c42f09d3b0997742e2048
thuynh02 <th1142@nyu.edu>
2015-01-09 18:20:46 -0500

    Reused block code and made a floor to start off

---
+++ block.py
@@ -0,0 +1,29 @@
+"""
+	Levels are generated as blocks of various types
+"""
+
+# Imports
+import pygame
+TYPE_ONE_BLACK = (20, 25, 55)
+
+
+class Block(pygame.sprite.Sprite):
+
+	# -----------------------------------------------------------------------
+	# Constructor
+	def __init__(self, x, y,  width, height, color=TYPE_ONE_BLACK, block_type=1):
+		super(Block, self).__init__()
+
+		self.image = pygame.Surface((width, height))
+		self.image.fill(color)
+		self.rect = self.image.get_rect()
+		# self.originx = self.rect.centerx
+		# self.originy = self.rect.centery
+		self.rect.x = x  # -self.originx
+		self.rect.y = y  # -self.originy
+		self.block_type = block_type
+
+	# -----------------------------------------------------------------------
+	# Mutators
+	def set_image(self, filename):
+			self.image = pygame.image.load(filename).convert()
--- main.py
+++ main.py
@@ -1,24 +1,23 @@
-__author__ = 'Tony'
+"""
+	This file is responsible for combining game logic and
+	loading content onto display
+"""
 import pygame
+from block import *
 WINDOW_WIDTH = 800
 WINDOW_HEIGHT = 600
 FRAMERATE_CAP = 60
-BACKGROUND_COLOR = (105, 192, 255)
-
-
-# initializes pygame and window properties.
-def startup():
-	pygame.init()
-	window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), pygame.RESIZABLE)
-	pygame.display.set_caption("Game")
-	window.fill(BACKGROUND_COLOR)
+BACKGROUND_COLOR = (105, 192, 255)  # skyblue color
 
 
 def main():
-	startup()
-	clock = pygame.time.Clock()
+	pygame.init()
+	pygame.display.set_caption("Game")
+	window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), pygame.RESIZABLE)
+	window.fill(BACKGROUND_COLOR)
 
 	running = True
+	clock = pygame.time.Clock()
 	try:
 		while running:
 			for event in pygame.event.get():
@@ -27,6 +26,12 @@ def main():
 
 			# ===== update
 			clock.tick(FRAMERATE_CAP)
+
+			# block test (floor)
+			block_list = pygame.sprite.Group()
+			for x in range(20):
+				block_list.add(Block(0+(40*x), 500, 40, 100))  # 40 x 100 blocks to create a "floor"
+			block_list.draw(window)
 
 			# level
 			# player
